e87bc094e7120143d7bb3e19710e325699a31e29
Cristina Silva <crissilva.eng@gmail.com>
2018-11-19 05:05:46 -0200

    :rocket: (offensive): Implement websocket to send offensive between players.

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -1,8 +1,56 @@
+const one = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+const two = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+const battle = window.location.pathname
+const socket = io()
+
+socket.emit('battle.join', { battle })
+socket.on('battle.offensive', target => {
+  console.log(target)
+})
+
+const getTarget = target => {
+  const [_, position] = target.split('-')
+  const value = parseInt(position)
+  const row = parseInt(value / 10)
+  const column = parseInt(value % 10)
+  return { row, column, positon: `one-${position}` }
+}
+
+const onClickHandler = event => {
+  onPositionClick.play()
+  const target = getTarget(event.target.id)
+  socket.emit('battle.offensive', { battle, target })
+}
+
 const onPositionHover = document.getElementById('on-position-hover')
 const onPositionClick = document.getElementById('on-position-click')
 
-const [...positions] = document.getElementsByClassName('default')
-positions.forEach(position => {
-  position.onmouseenter = () => onPositionHover.play()
-  position.onclick = () => onPositionClick.play()
-})
+const [...positions] = document.getElementsByClassName('position player-two default')
+positions.forEach(position => position.onmouseenter = () => onPositionHover.play())
+
+const [...opponent] = document.getElementsByClassName('position player-two')
+opponent.forEach(position => position.onclick = onClickHandler)
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express')
-const uuid = require('uuid/v4')
-const path = require('path')
-
 const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+const path = require('path')
+const uuid = require('uuid/v4')
+
 const port = process.env.PORT || 3000
 
 app.use('/:game', express.static(path.join(__dirname, 'public')))
 app.get('/', (_, res) => res.redirect(`${uuid()}`))
 
-app.listen(port, () => console.log(`Battleship running on  0.0.0.0:${port}!`))
+io.on('connection', (socket) => {
+  socket.on('battle.join', ({ battle }) => socket.join(battle))
+
+  socket.on('battle.offensive', ({ battle, target }) =>
+    socket.to(battle).emit('battle.offensive', target))
+})
+
+http.listen(port, () => console.log(`Battleship running on  0.0.0.0:${port}!`))
