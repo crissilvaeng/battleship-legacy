01bcbf3d8e229339add002d07795242c78628595
Cristina Silva <crissilva.eng@gmail.com>
2018-11-19 05:43:53 -0200

    :rocket: (offensive-water): Implements audio and source when oponent hit the water.

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -1,14 +1,14 @@
 const one = [
+  [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+  [0, 1, 0, 1, 0, 0, 0, 1, 1, 1],
+  [0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+  [0, 1, 0, 0, 0, 0, 1, 1, 0, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 1, 0, 0],
 ]
 
 const two = [
@@ -24,12 +24,35 @@ const two = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
+const mock = (player, field) => {
+  const n = [...Array(10).keys()]
+  n.forEach(row => n.forEach(column => {
+    if (field[row][column]) {
+      const id = `${player}-${row}${column}`
+      const position = document.getElementById(id)
+      position.classList.remove('water')
+      position.classList.add('ship')
+    }
+  }))
+}
+
+mock('one', one)
+
+const onPositionHover = document.getElementById('on-position-hover')
+const onPositionClick = document.getElementById('on-position-click')
+const onWaterOffensive = document.getElementById('on-water-offensive')
+
 const battle = window.location.pathname
 const socket = io()
 
 socket.emit('battle.join', { battle })
-socket.on('battle.offensive', target => {
-  console.log(target)
+socket.on('battle.offensive', ({ row, column, position: id }) => {
+  if (one[row][column]) {
+    return
+  }
+  onWaterOffensive.play()
+  const position = document.getElementById(id)
+  position.classList.remove('water')
 })
 
 const getTarget = target => {
@@ -37,7 +60,7 @@ const getTarget = target => {
   const value = parseInt(position)
   const row = parseInt(value / 10)
   const column = parseInt(value % 10)
-  return { row, column, positon: `one-${position}` }
+  return { row, column, position: `one-${position}` }
 }
 
 const onClickHandler = event => {
@@ -46,10 +69,7 @@ const onClickHandler = event => {
   socket.emit('battle.offensive', { battle, target })
 }
 
-const onPositionHover = document.getElementById('on-position-hover')
-const onPositionClick = document.getElementById('on-position-click')
-
-const [...positions] = document.getElementsByClassName('position player-two default')
+const [...positions] = document.getElementsByClassName('position player-two water')
 positions.forEach(position => position.onmouseenter = () => onPositionHover.play())
 
 const [...opponent] = document.getElementsByClassName('position player-two')
